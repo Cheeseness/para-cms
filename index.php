fea5566b6fcbaccd3860258a27237afd44266623
Josh Bush <contact@jbushproductions.com>
2012-05-18 06:06:52 +1000

    Closes #1, closes #3, fixes #8. Moved some initialisation/non-config stuff from config.php to index.php

--- index.php
+++ index.php
@@ -18,8 +18,14 @@ You should have received a copy of the GNU General Public License
 along with Para CMS.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+//This is a variable referenced in config.php. Don't play with it :3
+$externalLinks = array();
 
 include_once("config.php");
+
+//Sets the time zone if the time zone has been set in the config file. Don't play with this either.
+if( isset($timeZone) ) date_default_timezone_set($timeZone);
+
 include_once("functions.php");
 
 
@@ -51,7 +57,9 @@ echo "<hgroup>\n";
 echo "\t<h1><a href = 'index.php'>" . $siteTitle . "</a></h1>\n";
 echo "</hgroup>\n";
 
-echo getNavMenu($externalLinks);
+echo getNavMenu($externalLinks, $pageTitle);
+
+echo getContentsMenu($currentPage);
 ?>
 
 <div id = 'content'>
